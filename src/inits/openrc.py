fbbf2ecc90b02c00673ed59604779119c99e1349
Solirs <zcauchemar@gmail.com>
2022-06-04 19:17:48 +0200

    Added possibility to set own template path

--- src/inits/openrc.py
+++ src/inits/openrc.py
@@ -2,10 +2,10 @@
 from utils import color, pathutils
 
 class OpenRCgen():
-    def __init__(self, *args, **kwargs):
+    def __init__(self, template=None, *args, **kwargs):
         self.scrpath = "/etc/init.d/"
-        self.template = kwargs.get("custompath", pathutils.templatepath() + "/templates/openrc_initscript.txt")
+        self.template = pathutils.templatepath() + "/templates/openrc_initscript.txt"
+        if template: self.template = template #I would love to use kwargs here but they dont resort to default value if kwarg is none...
 
     def OpenRCgen(self, scrpath: str) -> None:
         print(color.Colors().brgreen + "[+] Generating for OpenRC")
-        print(self.template)
--- src/inits/systemd.py
+++ src/inits/systemd.py
@@ -3,8 +3,9 @@
 from utils import color, pathutils
 
 class SystemDgen():
-    def __init__(self, *args, **kwargs):
+    def __init__(self, template=None, *args, **kwargs):
         self.scrpath = "/etc/systemd/system/"
-        self.template = kwargs.get("custompath", pathutils.templatepath() + "/templates/systemd_service.txt")
+        self.template = pathutils.templatepath() + "/templates/systemd_service.txt" 
+        if template: self.template = template #I would love to use kwargs here but they dont resort to default value if kwarg is none...
     def SystemDgen(self, scrpath: str):
         print(color.Colors().brgreen +  "[+] Generating for SystemD")
