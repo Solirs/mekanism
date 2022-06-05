0633e4a5fbda7b1029d7243f3b06e2fcd57f16bd
Solirs <zcauchemar@gmail.com>
2022-06-05 23:51:28 +0200

    Added logutils red func

--- src/utils/logutils.py
+++ src/utils/logutils.py
@@ -6,5 +6,8 @@ def fatal(msg: str):
     print(color.Colors().red +"FATAL: " + msg + color.Colors().reset)
     sys.exit(1)
 
+def red(msg: str):
+    print(color.Colors().red + msg + color.Colors().reset)
+
 def good(msg: str):
     print(color.Colors().brgreen + msg + color.Colors().reset)
