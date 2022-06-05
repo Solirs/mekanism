9eea7bc0e093d5794a4f42c20605c109e2e00b82
Solirs <zcauchemar@gmail.com>
2022-06-05 22:50:57 +0200

    Added good log utility

--- src/utils/logutils.py
+++ src/utils/logutils.py
@@ -5,3 +5,6 @@ from utils import color
 def fatal(msg: str):
     print(color.Colors().red +"FATAL: " + msg + color.Colors().reset)
     sys.exit(1)
+
+def good(msg: str):
+    print(color.Colors().brgreen + msg + color.Colors().reset)
