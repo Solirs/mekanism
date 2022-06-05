67a5db36f8e44b12fb7baf4098713dc1279f88ed
Solirs <zcauchemar@gmail.com>
2022-06-05 23:50:38 +0200

    Fixed the templates to contain pidfiles and separated flags (so the script actually runs!)

--- src/preprocessor/preprocessor.py
+++ src/preprocessor/preprocessor.py
@@ -1,15 +1,19 @@
-from utils import color
+from utils import color, logutils
 
 def get_prog_name(cmd: str):
     if "/" in cmd.split()[0]:
         return cmd.split()[0].split("/")[-1]
 
+def get_prog_flags(cmd: str):
+    return "".join(cmd.split()[1:]).rstrip()
 
 
 def proprocess_template(template: str, cmd: str):
-    print(color.Colors().brgreen + "[+] Filling template..." + color.Colors().reset)
+    logutils.good("[+] Filling template...")
     progname = get_prog_name(cmd)
-    final = template.replace("$MK_SVNAME", progname).replace("$MK_EXECCMD", f'''"{cmd}"''')
+    flags = get_prog_flags(cmd)
+    final = template.replace("$MK_SVNAME", progname).replace("$MK_EXECCMD", f'"{cmd.split()[0]}"').replace("$MK_FLAGS", flags)
+    
     return final
     
 
